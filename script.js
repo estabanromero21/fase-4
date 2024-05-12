c870c0efd235eeef970b3ed7feeae7ef72ef9ed3
unknown <tokyosasuke15@gmail.com>
2024-05-12 14:53:23 -0500

    proyecto_react

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('exploreBtn').addEventListener('click', function() {
+    alert('¡Explorar ahora!'); // Esto se puede cambiar por una acción más interactiva, como redirigir a una página de recursos.
+});
+
